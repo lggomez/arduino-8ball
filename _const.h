03f19a48fdbc1d936b8e191123bb358d83cda1c7
Luis GG <luis.gg1990@gmail.com>
2019-04-23 23:13:35 -0300

    Reestructure the whole project and add all missing and updated deps

---
+++ _const.h
@@ -0,0 +1,36 @@
+#define DEFAULT_DRAW_COLOR 1
+#define SH1106_WIDTH 128
+#define SH1106_HEIGHT 64
+
+#define FONT u8g2_font_courB10_tf
+#define FONT_WIDTH 11
+#define FONT_HEIGTH 16
+#define UPPER_MARGIN 8
+#define LOWER_MARGIN 8
+
+#define REFRESH_INTERVAL 250
+
+#define FIZZLEFADE_BUFSIZE 136
+
+#define MAX_MESSAGE_LENGTH 24
+
+/*
+-----------------DEBUG FLAGS AND FEATURE START
+*/
+#ifdef  DEBUG
+  #define DEBUG_PRINT(x) Serial.print(x)
+  #define DEBUG_PRINTLN(x) Serial.println(x)
+#else
+  #define DEBUG_PRINT(x)
+  #define DEBUG_PRINTLN(x)
+#endif
+
+#define OUTPUT_NORMALIZED_VECTORS 0
+#define OUTPUT_ACCEL_VECTORS 0
+#define OUTPUT_INVALID_MPU_STATE 0
+#define OUTPUT_DISPLAY_MESSAGE 1
+#define OUTPUT_MPU_FIFO_OVERFLOW 1
+#define PAUSE_ON_OVERFLOW 0
+/*
+-----------------DEBUG FLAGS AND FEATURE END
+*/
